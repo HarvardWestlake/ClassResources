8df3b97218f5eecc45e9916088d6317b9a9fb939
jreiner16 <joereiner16@gmail.com>
2025-11-06 08:16:51 -0800

    Moved some stuff around and improved vite

--- public/static/music/widgets/rhythm-reading/vite.config.js
+++ public/static/music/widgets/rhythm-reading/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     server: {
         host: '127.0.0.1',
         port: 5173,
-        strictPort: true,
+        strictPort: false, // Auto-find available port if 5173 is taken
     },
     build: {
         outDir: 'dist',
